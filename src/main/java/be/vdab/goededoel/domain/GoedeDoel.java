2c9bc76655c0c35dc45c58fcbb789a6c6ee2c8ff
Birger Huysmans <birger.huysmans@gmail.com>
2021-03-30 15:27:15 +0200

    Commit 3

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class GoedeDoel {
 	private final String naam;
-	private BigDecimal opgebracht;
+	private BigDecimal opbrengst = BigDecimal.ZERO;
 
 	public GoedeDoel(String naam) {
 		this.naam = naam;
@@ -14,7 +14,7 @@ public class GoedeDoel {
 		return naam;
 	}
 
-	public BigDecimal getOpgebracht() {
-		return opgebracht;
+	public BigDecimal getOpbrengst() {
+		return opbrengst;
 	}
 }
